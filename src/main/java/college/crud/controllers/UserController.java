54c366f9d2458c672b33d0d1f43cc30035f4189b
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-18 11:59:42 -0300

    feat: user controller finding user by id

--- src/main/java/college/crud/controllers/UserController.java
+++ src/main/java/college/crud/controllers/UserController.java
@@ -1,12 +1,15 @@
 package college.crud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import college.crud.exception.ResourceNotFoundException;
 import college.crud.models.User;
 import college.crud.repositories.UsersRepository;
 
@@ -19,5 +22,12 @@ public class UserController {
 	@GetMapping("users")
 	public List<User> getAllUsers() {
 		return this.usersRepository.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable(value="id")String userId) throws ResourceNotFoundException{
+		User user = usersRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with the id: " + userId));
+		return ResponseEntity.ok().body(user);
 	}
 }
